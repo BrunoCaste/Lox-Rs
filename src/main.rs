5f02e0d4e563f77cae7d3d339181243223cf5e3e
Bruno Castellano <bruno.castellano@mi.unc.edu.ar>
2022-12-23 23:17:25 -0300

    Added repl and script execution (only for expressions)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,82 @@
 #![feature(is_some_and)]
 
-mod lexer;
+use std::{
+    env,
+    fs::read_to_string,
+    io::{stdin, stdout, Write},
+    process::ExitCode,
+};
+
+use ast::Parser;
+use lexer::Lexer;
+use parser::RecursiveDescent;
 
 mod ast;
+mod lexer;
 mod parser;
 
-fn main() {
-    println!("Hello, world!");
+fn usage(prog: String) -> ExitCode {
+    eprintln!("Usage: {prog} [script]");
+    ExitCode::from(64)
+}
+
+fn run_file(path: &str) -> ExitCode {
+    let src = match read_to_string(path) {
+        Ok(src) => src,
+        Err(_) => {
+            println!("ERROR: unable to open file: {path}");
+            return ExitCode::from(74);
+        }
+    };
+    run(&src).unwrap_or(ExitCode::from(0))
+}
+
+fn repl() -> ExitCode {
+    let stdin = stdin();
+    let mut input = String::with_capacity(64);
+    loop {
+        input.clear();
+        print!("> ");
+        stdout().flush().expect("Error flushing stdout");
+        stdin
+            .read_line(&mut input)
+            .expect("Error reading from stdin");
+
+        if let Some(e) = run(&input) {
+            return e;
+        }
+    }
+}
+
+fn run(src: &str) -> Option<ExitCode> {
+    let mut lexer = Lexer::new(src.chars());
+    let expr = match RecursiveDescent::parse(&mut lexer) {
+        Ok(e) => e,
+        e => {
+            println!("syntax error\t{e:?}");
+            return Some(ExitCode::from(1));
+        }
+    };
+
+    match expr.eval() {
+        Ok(v) => {
+            println!("{v}");
+            None
+        }
+        e => {
+            println!("runtime error\t{e:?}");
+            Some(ExitCode::from(1))
+        }
+    }
+}
+
+fn main() -> ExitCode {
+    let mut args = env::args();
+    let prog = args.next().expect("Program name must always be present");
+    let args: Vec<_> = args.collect();
+    match &args[..] {
+        [] => repl(),
+        [script] => run_file(script),
+        _ => usage(prog),
+    }
 }
